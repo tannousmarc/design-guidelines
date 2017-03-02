bd3c740cf36768e9cc9f73a84ef28862a5e973b2
Marc Tannous <tannous.marc@gmail.com>
2017-03-02 00:20:20 +0000

    git init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  $(".clickable").click(function() {
+    $(this).toggleClass("buttonPress");
+  });
+});
+function togglePadding(){
+  $("#content").toggleClass("enablePadding");
+}
+function toggleLineHeight(){
+  $("#content").toggleClass("enableLineHeight");
+}
+function toggleFonts(){
+  $("#content h1").toggleClass("enableFontsH1");
+  $("#content p").toggleClass("enableFontsP");
+}
+function toggleSizing(){
+  $("#content h1").toggleClass("enableSizeH1");
+  $("#content p").toggleClass("enableSizeP");
+}
